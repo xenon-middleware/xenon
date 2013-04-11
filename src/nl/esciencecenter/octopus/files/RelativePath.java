21621f435f741821f8cc263085b7fd3c5fea63bc
jmaassen <J.Maassen@esciencecenter.nl>
2013-04-11 17:11:00 +0200

    Added hashcode and equals to RelativePath.

--- src/nl/esciencecenter/octopus/files/RelativePath.java
+++ src/nl/esciencecenter/octopus/files/RelativePath.java
@@ -353,6 +353,34 @@ public class RelativePath {
         return new RelativePath(tmp, seperator);
     }
     
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(elements);
+        result = prime * result + ((seperator == null) ? 0 : seperator.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RelativePath other = (RelativePath) obj;
+        if (!Arrays.equals(elements, other.elements))
+            return false;
+        if (seperator == null) {
+            if (other.seperator != null)
+                return false;
+        } else if (!seperator.equals(other.seperator))
+            return false;
+        return true;
+    }
+
     public String toString() {
         return getPath();
     }
