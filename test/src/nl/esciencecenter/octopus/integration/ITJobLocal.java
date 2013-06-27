c0f1d97b11d3a6d6b61b37a6dd2f612e384db3e5
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2013-06-27 14:09:57 +0200

    Cleanup test: * Add javadoc * Do formatting * Remove directory when done * Use logger instead of System.out
    Refs #109.

--- test/src/nl/esciencecenter/octopus/integration/ITJobLocal.java
+++ test/src/nl/esciencecenter/octopus/integration/ITJobLocal.java
@@ -17,47 +17,58 @@
 package nl.esciencecenter.octopus.integration;
 
 import static org.junit.Assert.*;
- 
+
 import java.nio.file.Files;
 import java.nio.file.Path;
- 
+
 import nl.esciencecenter.octopus.Octopus;
 import nl.esciencecenter.octopus.OctopusFactory;
 import nl.esciencecenter.octopus.jobs.Job;
 import nl.esciencecenter.octopus.jobs.JobDescription;
 import nl.esciencecenter.octopus.jobs.Scheduler;
- 
+
+import org.junit.Rule;
 import org.junit.Test;
- 
+import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ITJobLocal {
- 
-@Test
-public void WorkingDirectoryRelativlyToCwd() throws Exception {
-Octopus octopus = OctopusFactory.newOctopus(null);
-Path testdir = Files.createTempDirectory("octopustest");
-System.out.println("Absolute tmp dir "+testdir.toString());
- 
-JobDescription description = new JobDescription();
-description.setExecutable("/usr/bin/touch");
-description.setArguments("bla");
-description.setQueueName("single");
-// testdir == /tmp/octopustest<random>/
-description.setWorkingDirectory(testdir.toString());
- 
-Scheduler scheduler = octopus.jobs().getLocalScheduler();
-Job job = octopus.jobs().submitJob(scheduler, description);
- 
-octopus.jobs().waitUntilDone(job, 5000);
- 
-//Expected files in /tmp/octopustest<random>/
-assertTrue(Files.exists(testdir.resolve("bla")));
- 
-//Assertions below fail, as files are in $PWD/tmp/octopustest<random>/
-System.out.println("stdout written to "+testdir.resolve("stdout.txt"));
-assertTrue(Files.exists(testdir.resolve("stdout.txt")));
-assertTrue(Files.exists(testdir.resolve("stderr.txt")));
- 
-// TODO clean up
-octopus.end();
-}
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    final Logger logger = LoggerFactory.getLogger(ITJobLocal.class);
+
+    /**
+     * Regression test for issue #109
+     *
+     * @throws Exception
+     */
+    @Test
+    public void WorkingDirectoryRelativlyToCwd() throws Exception {
+        Octopus octopus = OctopusFactory.newOctopus(null);
+        Path testdir = testFolder.newFolder("octopustest").toPath();
+        // testdir == /tmp/junit<random>/octopustest/
+        logger.info("Absolute tmp dir {}", testdir);
+
+        JobDescription description = new JobDescription();
+        description.setExecutable("/usr/bin/touch");
+        description.setArguments("bla");
+        description.setQueueName("single");
+        description.setWorkingDirectory(testdir.toString());
+
+        Scheduler scheduler = octopus.jobs().getLocalScheduler();
+        Job job = octopus.jobs().submitJob(scheduler, description);
+
+        octopus.jobs().waitUntilDone(job, 5000);
+
+        // Expect files in /tmp/junit<random>/octopustest/
+        assertTrue(Files.exists(testdir.resolve("bla")));
+
+        logger.info("stdout written to {}", testdir.resolve("stdout.txt"));
+        assertTrue(Files.exists(testdir.resolve("stdout.txt")));
+        assertTrue(Files.exists(testdir.resolve("stderr.txt")));
+
+        octopus.end();
+    }
 }
