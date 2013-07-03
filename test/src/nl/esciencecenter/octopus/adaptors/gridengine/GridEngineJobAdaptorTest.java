17794869f733e2a70b3f1be5bb93af0aaab53902
Niels Drost <n.drost@esciencecenter.nl>
2013-07-03 11:49:15 +0200

    Added support for a custom job script. Also add a gridengine specific test, and made a few minor changes to the generic test framework to accomodate tests in subclasses. Fixes 121.

--- test/src/nl/esciencecenter/octopus/adaptors/gridengine/GridEngineJobAdaptorTest.java
+++ test/src/nl/esciencecenter/octopus/adaptors/gridengine/GridEngineJobAdaptorTest.java
@@ -16,24 +16,91 @@
 
 package nl.esciencecenter.octopus.adaptors.gridengine;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.OutputStream;
+
 import nl.esciencecenter.octopus.adaptors.GenericJobAdaptorTestParent;
+import nl.esciencecenter.octopus.files.AbsolutePath;
+import nl.esciencecenter.octopus.files.FileSystem;
+import nl.esciencecenter.octopus.files.OpenOption;
+import nl.esciencecenter.octopus.files.RelativePath;
+import nl.esciencecenter.octopus.jobs.Job;
+import nl.esciencecenter.octopus.jobs.JobDescription;
+import nl.esciencecenter.octopus.jobs.JobStatus;
+import nl.esciencecenter.octopus.jobs.Scheduler;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 /**
  * @author Niels Drost <N.Drost@esciencecenter.nl>
- *
+ * 
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GridEngineJobAdaptorTest extends GenericJobAdaptorTestParent {
-    
+
     @BeforeClass
-    public static void prepareGridEngineJobAdaptorTest() throws Exception { 
+    public static void prepareGridEngineJobAdaptorTest() throws Exception {
         GenericJobAdaptorTestParent.prepareClass(new GridEngineJobTestConfig(null));
     }
 
     @AfterClass
-    public static void cleanupGridEngineJobAdaptorTest() throws Exception { 
+    public static void cleanupGridEngineJobAdaptorTest() throws Exception {
         GenericJobAdaptorTestParent.cleanupClass();
+    }
+
+    @org.junit.Test
+    public void ge_test01_jobWithCustomScript() throws Exception {
+        String message = "Hello World! test01\n";
+        
+        String workingDir = getWorkingDir("ge_test01");
+        
+        Scheduler scheduler = config.getDefaultScheduler(jobs, credentials);
+        FileSystem filesystem = config.getDefaultFileSystem(files, credentials);
+        
+        AbsolutePath root = filesystem.getEntryPath().resolve(new RelativePath(workingDir));
+        files.createDirectories(root);
+        
+        AbsolutePath script = root.resolve(new RelativePath("script"));
+        AbsolutePath stdout = root.resolve(new RelativePath("stdout.txt"));
+        
+        String scriptContent = "#!/bin/bash\n" +
+                "#$ -o " + stdout.getPath() + "\n"
+                + "#$ -e /dev/null\n"
+                + "echo " + message;
+
+        OutputStream out = files.newOutputStream(script, OpenOption.CREATE, OpenOption.APPEND, OpenOption.WRITE);
+        writeFully(out, scriptContent);
+        
+        JobDescription description = new JobDescription();
+        description.setInteractive(false);
+        description.getJobOptions().put("ge.job.script", script.getPath());
+        //FIXME: limitation of ge adaptor.
+        description.setExecutable("ignored");
+        
+        Job job = jobs.submitJob(scheduler, description);
+        JobStatus status = jobs.waitUntilDone(job, 60000);
+        
+        if (!status.isDone()) { 
+            throw new Exception("Job exceeded deadline!");
+        }
+        
+        if (status.hasException()) {
+            throw new Exception("Job failed!", status.getException());
+        }
+        
+        String outputContent = readFully(files.newInputStream(stdout));
+
+        files.delete(stdout);
+        files.delete(script);           
+        files.delete(root);        
+        
+        jobs.close(scheduler);
+        files.close(filesystem);
+        
+        assertTrue(outputContent.equals(message));
     }
 }
