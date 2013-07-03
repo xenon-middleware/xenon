8c8abc8b73ebb9474566f1b41cb171e89d9575ec
jmaassen <J.Maassen@esciencecenter.nl>
2013-07-03 14:21:39 +0200

    Added first example. Removed unused test and util classes.

---
+++ examples/src/nl/esciencecenter/octopus/examples/CreatingOctopus.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013 Netherlands eScience Center
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.esciencecenter.octopus.examples;
+
+import nl.esciencecenter.octopus.Octopus;
+import nl.esciencecenter.octopus.OctopusFactory;
+import nl.esciencecenter.octopus.credentials.Credentials;
+import nl.esciencecenter.octopus.exceptions.OctopusException;
+import nl.esciencecenter.octopus.files.Files;
+import nl.esciencecenter.octopus.jobs.Jobs;
+
+/**
+ * A simple example of how to create an octopus and how to retrieve the various interfaces.
+ * 
+ * @author Jason Maassen <J.Maassen@esciencecenter.nl>
+ * @version 1.0
+ * @since 1.0
+ */
+public class CreatingOctopus {
+
+    @SuppressWarnings("unused")
+    public void main(String [] args) { 
+        
+        try { 
+
+            // We create a new octopus using the OctopusFactory. 
+            Octopus octopus = OctopusFactory.newOctopus(null);
+
+            // Next, we retrieve the Files, Jobs and Credentials API
+            Files files = octopus.files();
+            Jobs jobs = octopus.jobs();
+            Credentials credentials = octopus.credentials();
+            
+            // We can now uses the interfaces to get some work done!
+            // ....
+            
+            // Finally, we end octopus to release all resources 
+            OctopusFactory.endOctopus(octopus);
+            
+        } catch (OctopusException e) { 
+            System.out.println("Example failed: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
