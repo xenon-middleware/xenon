6f9dd11aabab106d8158c364aa210fb99b4e9b90
Christiaan Meijer <c.meijer@esciencecenter.nl>
2015-05-20 16:36:50 +0200

    removed some tests that only work locally

--- test/src/nl/esciencecenter/xenon/adaptors/ftp/FTPFileAdaptorTest.java
+++ test/src/nl/esciencecenter/xenon/adaptors/ftp/FTPFileAdaptorTest.java
@@ -16,32 +16,16 @@
 
 package nl.esciencecenter.xenon.adaptors.ftp;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.nio.file.Paths;
-
-import nl.esciencecenter.xenon.XenonException;
 import nl.esciencecenter.xenon.adaptors.GenericFileAdaptorTestParent;
-import nl.esciencecenter.xenon.files.DirectoryStream;
-import nl.esciencecenter.xenon.files.FileAttributes;
-import nl.esciencecenter.xenon.files.Path;
-import nl.esciencecenter.xenon.files.RelativePath;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Test;
 
 /**
  * @author Christiaan Meijer <C.Meijer@esciencecenter.nl>
  *
  */
 public class FTPFileAdaptorTest extends GenericFileAdaptorTestParent {
-
-    private String testDirectoryPath = "test123";
-    private String testFilePath = "somefile2";
-    private String testLinkPath = "somelink";
-    private String executableTestFile = "execfile";
 
     @BeforeClass
     public static void prepareFTPFileAdaptorTest() throws Exception {
@@ -51,163 +35,5 @@ public class FTPFileAdaptorTest extends GenericFileAdaptorTestParent {
     @AfterClass
     public static void cleanupFTPFileAdaptorTest() throws Exception {
         GenericFileAdaptorTestParent.cleanupClass();
-    }
-
-    @Test
-    public void exists_existingDirectory_true() throws Exception {
-        assertTrue(ftpClientCallExists("test123"));
-    }
-
-    @Test
-    public void exists_existingFile_true() throws Exception {
-        assertTrue(ftpClientCallExists("somefile"));
-    }
-
-    @Test
-    public void exists_nonExistingPath_false() throws Exception {
-        assertFalse(ftpClientCallExists("nosuchfile48566393"));
-    }
-
-    private boolean ftpClientCallExists(String relativePath) throws Exception, XenonException {
-        prepare();
-        Path root = config.getWorkingDir(files, credentials);
-        Path path = resolve(root, relativePath);
-        boolean exists = files.exists(path);
-        cleanup();
-        return exists;
-    }
-
-    @Test
-    public void getAttributes_ofFile_isFile() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testFilePath);
-        assertTrue(attributes.isRegularFile());
-    }
-
-    @Test
-    public void getAttributes_ofFile_isNotDirectory() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testFilePath);
-        assertFalse(attributes.isDirectory());
-    }
-
-    @Test
-    public void getAttributes_ofFile_isNotLink() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testFilePath);
-        assertFalse(attributes.isSymbolicLink());
-    }
-
-    @Test
-    public void getAttributes_ofDirectory_isDirectory() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testDirectoryPath);
-        assertTrue(attributes.isDirectory());
-    }
-
-    @Test
-    public void getAttributes_ofDirectory_isNotFile() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testDirectoryPath);
-        assertFalse(attributes.isRegularFile());
-    }
-
-    @Test
-    public void getAttributes_ofDirectory_isNotLink() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testDirectoryPath);
-        assertFalse(attributes.isSymbolicLink());
-    }
-
-    @Test
-    public void getAttributes_ofRoot_isDirectory() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes("/");
-        assertTrue(attributes.isDirectory());
-    }
-
-    @Test
-    public void getAttributes_ofLink_isNotDirectory() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testLinkPath);
-        assertFalse(attributes.isDirectory());
-    }
-
-    @Test
-    public void getAttributes_ofLink_isNotFile() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testLinkPath);
-        assertFalse(attributes.isRegularFile());
-    }
-
-    @Test
-    public void getAttributes_ofLink_isLink() throws Exception {
-        FileAttributes attributes = ftpClientGetAttributes(testLinkPath);
-        assertTrue(attributes.isSymbolicLink());
-    }
-
-    private FileAttributes ftpClientGetAttributes(String relativePath) throws Exception {
-        prepare();
-        Path root = config.getWorkingDir(files, credentials);
-        Path path = resolve(root, relativePath);
-        FileAttributes attributes = files.getAttributes(path);
-        cleanup();
-        return attributes;
-    }
-
-    @Test
-    public void createAndDeleteDir_doesNotExist() throws Exception {
-        prepare();
-        String relativePath = "newlycreated";
-        Path root = config.getWorkingDir(files, credentials);
-        Path path = resolve(root, relativePath);
-        files.createDirectory(path);
-        files.delete(path);
-        boolean exists = files.exists(path);
-        cleanup();
-        assertFalse(exists);
-    }
-
-    @Test
-    public void createDirs_1dir() throws Exception {
-        // Arrange
-        prepare();
-        String nestedDir = "nesteddir";
-        String parentDir = "parentdir";
-        String relativePath = Paths.get(parentDir).resolve(nestedDir).toString();
-        Path root = config.getWorkingDir(files, credentials);
-        Path nestedPath = resolve(root, relativePath);
-
-        // Act
-        files.createDirectories(nestedPath);
-        boolean exists = files.exists(nestedPath);
-
-        // Cleanup
-        Path parentPath = resolve(root, parentDir);
-        files.delete(nestedPath);
-        files.delete(parentPath);
-        cleanup();
-
-        // Assert
-        assertTrue(exists);
-    }
-
-    @Test
-    public void newDirectoryStream_root_returnList() throws Exception {
-        prepare();
-        Path dir = config.getWorkingDir(files, credentials);
-        DirectoryStream<Path> newDirectoryStream = files.newDirectoryStream(dir);
-        boolean notNull = newDirectoryStream != null;
-        cleanup();
-        assertTrue(notNull);
-    }
-
-    @Test
-    public void getAttributes_executableFile_isExecutable() throws Exception {
-        // Arrange
-        prepare();
-        Path root = config.getWorkingDir(files, credentials);
-        RelativePath relativePath = root.getRelativePath().resolve(executableTestFile);
-        Path testFilePath = files.newPath(root.getFileSystem(), relativePath);
-
-        // Act
-        FileAttributes attributes = files.getAttributes(testFilePath);
-
-        // Cleanup
-        cleanup();
-
-        // Assert
-        assertTrue(attributes.isExecutable());
     }
 }
