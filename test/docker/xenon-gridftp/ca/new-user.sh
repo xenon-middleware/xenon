076e5216ff5069e522c04c3c0bba97191abda55d
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-08-25 17:30:38 +0200

    Add passphrase to user globus certificate.

--- test/docker/xenon-gridftp/ca/new-user.sh
+++ test/docker/xenon-gridftp/ca/new-user.sh
@@ -8,4 +8,6 @@ grid-cert-request -cn $1 -nopassphrase -dir .
 grid-ca-sign -passin pass:$CA_PASSPHRASE -days $CA_DAYS -in usercert_request.pem -out usercert.pem
 # Convert to PKCS#1 format so jglobus can read key
 openssl rsa -in userkey.pem -out userkey.rsa.pem
+ssh-keygen -p -P "" -N $2 -f userkey.rsa.pem
+chmod go-r userkey.rsa.pem
 chown -R $MYUID /var/lib/globus /etc/grid-security
