f19659b045dd4ed7632ef5a1ce69a69283deb3f7
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-09-28 16:36:49 +0200

    Generate CA only if not yet generated; fixed run scripts

--- src/integrationTest/docker/run-tests.sh
+++ src/integrationTest/docker/run-tests.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-if [ "x$MYUID" -ne "x$(id xenon -u)" ]
-then
-echo "Changing uid of xenon user to MYUID=$MYUID"
-usermod -u $MYUID xenon
-chown -R $MYUID /home/xenon
+# Testing if UID was empty (not allowed)
+if [ "$MYUID" = "" ]; then
+	echo "\$MYUID is not set"
+	exit 1
+elif [ "$MYUID" = "$(id xenon -u)" ]; then
+	echo "Uid of xenon user is already same as MYUID=$MYUID, not changing uid"
 else
-echo "Uid of xenon user is already same as MYUID=$MYUID, not changing uid"
+	echo "Changing uid of xenon user to MYUID=$MYUID"
+	usermod -u $MYUID xenon
+	chown -R $MYUID /home/xenon
 fi
 
 # ssh in prepareIntegrationTest in build.gradle adds ecdsa key which it cant read
