715af04027fc12ca7c51b0ab0264c2118ea7714f
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2015-11-17 13:21:43 +0100

    Added task to check files for license header.

--- src/main/java/nl/esciencecenter/xenon/engine/files/CopyImplementation.java
+++ src/main/java/nl/esciencecenter/xenon/engine/files/CopyImplementation.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright 2013 Netherlands eScience Center
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package nl.esciencecenter.xenon.engine.files;
 
 import nl.esciencecenter.xenon.files.Copy;
