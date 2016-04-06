a5aa6e865796dff7870a94ac8bb3c460cf6ea0a6
Christiaan Meijer <c.meijer@esciencecenter.nl>
2016-04-06 11:00:25 +0200

    Removed temporary webdav tests (used for developing)

--- src/integrationTest/java/nl/esciencecenter/xenon/adaptors/webdav/WebdavFileAdaptorTest.java
+++ src/integrationTest/java/nl/esciencecenter/xenon/adaptors/webdav/WebdavFileAdaptorTest.java
@@ -16,20 +16,10 @@
 
 package nl.esciencecenter.xenon.adaptors.webdav;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
 
-import nl.esciencecenter.xenon.XenonException;
 import nl.esciencecenter.xenon.adaptors.GenericFileAdaptorTestParent;
-import nl.esciencecenter.xenon.files.FileAttributes;
-import nl.esciencecenter.xenon.files.FileSystem;
-import nl.esciencecenter.xenon.files.Path;
-import nl.esciencecenter.xenon.files.RelativePath;
 
 /**
  * @author Christiaan Meijer <C.Meijer@esciencecenter.nl>
@@ -54,135 +44,4 @@ public class WebdavFileAdaptorTest extends GenericFileAdaptorTestParent {
     public static void cleanupFTPFileAdaptorTest() throws Exception {
         GenericFileAdaptorTestParent.cleanupClass();
     }
-
-    @Test
-    public void getAttributes_dir_noThrow() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(DIR_PATH));
-        files.getAttributes(path);
-    }
-
-    @Test
-    public void getAttributes_dir_isDir() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(DIR_PATH));
-        FileAttributes attributes = files.getAttributes(path);
-        assertTrue(attributes.isDirectory());
-    }
-
-    @Test
-    public void getAttributes_file_isFile() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(FILE_PATH));
-        FileAttributes attributes = files.getAttributes(path);
-        assertTrue(attributes.isRegularFile());
-    }
-
-    @Test
-    public void getAttributes_file_noThrow() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(FILE_PATH));
-        files.getAttributes(path);
-    }
-
-    @Test(expected = XenonException.class)
-    public void getAttributes_nonExistent_throw() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NONEXISTENT_PATH));
-        files.getAttributes(path);
-    }
-
-    @Test
-    public void exists_nonExistent_returnFalse() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NONEXISTENT_PATH));
-        assertFalse(files.exists(path));
-    }
-
-    @Test
-    public void exists_filePath_returnTrue() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(FILE_PATH));
-        assertTrue(files.exists(path));
-    }
-
-    @Test
-    public void exists_dirPath_returnTrue() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(DIR_PATH));
-        assertTrue(files.exists(path));
-    }
-
-    @Test
-    public void createFile_newFile_fileExists() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NEWFILE_PATH));
-        files.createFile(path);
-        assertTrue(files.exists(path));
-        files.delete(path);
-    }
-
-    @Test
-    public void createDir_newDir_fileExists() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NEWDIR_PATH));
-        files.createDirectory(path);
-        assertTrue(files.exists(path));
-        files.delete(path);
-    }
-
-    @Test
-    public void delete_newFile_noFileExists() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NEWFILE_PATH));
-        files.createFile(path);
-        files.delete(path);
-        assertFalse(files.exists(path));
-    }
-
-    @Test
-    public void delete_newDir_noDirectoryExists() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NEWDIR_PATH));
-        files.createDirectory(path);
-        files.delete(path);
-        assertFalse(files.exists(path));
-    }
-
-    @Ignore
-    @Test(expected = XenonException.class)
-    public void delete_dirWithFile_throw() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path dirPath = files.newPath(fs, new RelativePath(NEWDIR_PATH));
-        Path filePath = files.newPath(fs, new RelativePath(NEWFILE_IN_NEWDIR_PATH));
-        files.createDirectory(dirPath);
-        files.createFile(filePath);
-        files.delete(dirPath);
-    }
-
-    @Test(expected = XenonException.class)
-    public void delete_nonExistent_throw() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NONEXISTENT_PATH));
-        files.delete(path);
-    }
-
-    @Test
-    public void createDir_newDirExistingParent_fileExists() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NEWDIR_PATH));
-        files.createDirectories(path);
-        assertTrue(files.exists(path));
-        files.delete(path);
-    }
-
-    @Test
-    public void createDir_newDirNonExistingParent_fileExists() throws Exception {
-        FileSystem fs = config.getTestFileSystem(files, credentials);
-        Path path = files.newPath(fs, new RelativePath(NONEXISTENTPARENT_NEWDIR_PATH));
-        files.createDirectories(path);
-        assertTrue(files.exists(path));
-        files.delete(path);
-    }
-
 }
