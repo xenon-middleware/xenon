c530246c2d882dd82d2efdfbcf2c214e28fb97d8
Jason Maassen <jmaassen73@gmail.com>
2016-07-19 14:18:25 +0200

    Added dummy cloud engine and implementation

---
+++ src/main/java/nl/esciencecenter/xenon/clouds/Cloud.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2013 Netherlands eScience Center
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.esciencecenter.xenon.clouds;
+
+import java.util.Map;
+
+/**
+ * @version 2.0
+ * @since 2.0
+ *
+ */
+public interface Cloud {
+
+    /**
+     * Get the name of the adaptor that created this Cloud.
+     * 
+     * @return the name of the adaptor.
+     */
+    String getAdaptorName();
+
+    /**
+     * Get the location of this Cloud.
+     * 
+     * @return the location of the Cloud.
+     */
+    String getLocation();
+
+    /**
+     * Get the scheme used to access the Cloud.
+     * 
+     * @return the scheme used to access the Cloud.
+     */
+    String getScheme();
+
+    /**
+     * Get the properties used to create this Cloud.
+     * 
+     * @return the properties used to create this Cloud.
+     */
+    Map<String, String> getProperties();
+    
+}
---
+++ src/main/java/nl/esciencecenter/xenon/engine/clouds/CloudImplementation.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2013 Netherlands eScience Center
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.esciencecenter.xenon.engine.clouds;
+
+import java.util.Map;
+
+import nl.esciencecenter.xenon.clouds.Cloud;
+
+/**
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public class CloudImplementation implements Cloud {
+
+    /* (non-Javadoc)
+     * @see nl.esciencecenter.xenon.clouds.Cloud#getAdaptorName()
+     */
+    @Override
+    public String getAdaptorName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see nl.esciencecenter.xenon.clouds.Cloud#getLocation()
+     */
+    @Override
+    public String getLocation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see nl.esciencecenter.xenon.clouds.Cloud#getScheme()
+     */
+    @Override
+    public String getScheme() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see nl.esciencecenter.xenon.clouds.Cloud#getProperties()
+     */
+    @Override
+    public Map<String, String> getProperties() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
