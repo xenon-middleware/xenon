45bf377ba101fb5f4332ccf0dff1280aa528d4aa
Jason Maassen <jmaassen73@gmail.com>
2016-09-05 14:02:59 +0200

    Updated test for VirtualMachineDescriptions

--- src/test/java/nl/esciencecenter/xenon/clouds/VirtualMachineDescriptionTest.java
+++ src/test/java/nl/esciencecenter/xenon/clouds/VirtualMachineDescriptionTest.java
@@ -18,7 +18,6 @@ package nl.esciencecenter.xenon.clouds;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
