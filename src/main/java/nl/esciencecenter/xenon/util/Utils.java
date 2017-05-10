1b4ba65ceb93d5379fc86e81e32ea32a20a3905a
Jason Maassen <jmaassen73@gmail.com>
2017-05-10 12:03:31 +0200

    Fixed html bug in Utils

--- src/main/java/nl/esciencecenter/xenon/util/Utils.java
+++ src/main/java/nl/esciencecenter/xenon/util/Utils.java
@@ -1208,10 +1208,10 @@ public final class Utils {
      * 
      * This allows the user to simply test if the deadline has passed by performing a check against the current epoch time:
      * 
-     *    <code>if (deadline <= System.currentTimeMillis()) { // deadline has passed }</code>
+     *    <code>if (deadline &lt;= System.currentTimeMillis()) { // deadline has passed }</code>
      * 
      * @param timeout
-     *          the timeout to compute the deadline with. Must be >= 0 or an IllegalArgumentException will be thrown.
+     *          the timeout to compute the deadline with. Must be &gt;= 0 or an IllegalArgumentException will be thrown.
      * @return
      *          the timestamp at which the timeout will expire, or <code>Long.MAX_VALUE</code> if the timeout causes an overflow.
      */
