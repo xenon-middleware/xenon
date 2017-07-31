2e2efc37cef485f3171a3150c6bf05a75985350a
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-07-31 11:24:50 +0200

    Fixed IntelliJ IDEA "Dangling Javadoc comment." warning
    By changing license header from javadoc to comment.

--- src/test/java/nl/esciencecenter/xenon/XenonPropertyDescriptionTest.java
+++ src/test/java/nl/esciencecenter/xenon/XenonPropertyDescriptionTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2013 Netherlands eScience Center
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,23 +17,23 @@ package nl.esciencecenter.xenon;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-
 import nl.esciencecenter.xenon.XenonPropertyDescription.Type;
+
+import org.junit.Test;
 
 public class XenonPropertyDescriptionTest {
 
 	@Test
 	public void test_name() {
 		XenonPropertyDescription p = new XenonPropertyDescription("NAME", Type.STRING, "EMPTY", "DESC");
-		assertEquals("NAME", p.getName());		
+		assertEquals("NAME", p.getName());
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void test_createFailsNameNull() {
 		new XenonPropertyDescription(null, Type.STRING, "EMPTY", "DESC");
 	}
-	
+
 
 	@Test(expected=IllegalArgumentException.class)
 	public void test_createFailsTypeNull() {
@@ -50,23 +50,23 @@ public class XenonPropertyDescriptionTest {
 		Type t = Type.valueOf("STRING");
 		assertEquals(Type.STRING, t);
 	}
-	
+
 	@Test
 	public void test_description() {
 		XenonPropertyDescription p = new XenonPropertyDescription("NAME", Type.STRING, "EMPTY", "DESC");
-		assertEquals("DESC", p.getDescription());		
+		assertEquals("DESC", p.getDescription());
 	}
 
 	@Test
 	public void test_default() {
 		XenonPropertyDescription p = new XenonPropertyDescription("NAME", Type.STRING, "EMPTY", "DESC");
-		assertEquals("EMPTY", p.getDefaultValue());		
+		assertEquals("EMPTY", p.getDefaultValue());
 	}
 
 	@Test
 	public void test_type() {
 		XenonPropertyDescription p = new XenonPropertyDescription("NAME", Type.STRING, "EMPTY", "DESC");
-		assertEquals(Type.STRING, p.getType());		
+		assertEquals(Type.STRING, p.getType());
 	}
-	
+
 }
