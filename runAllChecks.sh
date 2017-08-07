61f716d7660d5b8f4d7794864722f9c2f1df0509
Jason Maassen <jmaassen73@gmail.com>
2017-08-07 17:57:51 +0200

    Added script to run all checks

---
+++ runAllChecks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "+-------+"
+echo "| clean |"
+echo "+-------+"
+./gradlew clean
+
+echo "+------------+"
+echo "| unit tests |"
+echo "+------------+"
+./gradlew test
+
+echo "+--------------------+"
+echo "| fixed client tests |"
+echo "+--------------------+"
+./src/fixedClientEnvironmentTest/resources/run-fixed-client-environment-test.sh
+
+echo "+-------------------+"
+echo "| integration tests |"
+echo "+-------------------+"
+./gradlew integrationTest
+
+echo "+-----------------------------+"
+echo "| generate merged test report |"
+echo "+-----------------------------+"
+./gradlew jacocoMergedTestReport
