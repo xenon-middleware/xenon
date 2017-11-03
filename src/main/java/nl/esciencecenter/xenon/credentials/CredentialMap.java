edf52403d9b1904268fe5989140fb4bc5afae607
Jason Maassen <jmaassen73@gmail.com>
2017-11-03 13:31:11 +0100

    Reverted changes to CredentialMap

--- src/main/java/nl/esciencecenter/xenon/credentials/CredentialMap.java
+++ src/main/java/nl/esciencecenter/xenon/credentials/CredentialMap.java
@@ -15,17 +15,14 @@
  */
 package nl.esciencecenter.xenon.credentials;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * A {@link Credential} consisting of a collection of Credentials each uniquely identified by a String (typically a host name or host alias).
  *
  * A default Credential can be set that will be returned if by <code>get</code> if a key is not found.
  */
-public class CredentialMap implements Credential, Map<String, UserCredential> {
+public class CredentialMap implements Credential {
 
     private final HashMap<String, UserCredential> map = new HashMap<>();
 
@@ -97,61 +94,6 @@ public class CredentialMap implements Credential, Map<String, UserCredential> {
         }
 
         return defaultCredential;
-    }
-
-    @Override
-    public int size() {
-        return map.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return map.isEmpty();
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return map.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return map.containsValue(value);
-    }
-
-    @Override
-    public UserCredential get(Object key) {
-        return map.get(key);
-    }
-
-    @Override
-    public UserCredential remove(Object key) {
-        return map.remove(key);
-    }
-
-    @Override
-    public void putAll(Map<? extends String, ? extends UserCredential> m) {
-        map.putAll(m);
-    }
-
-    @Override
-    public void clear() {
-        map.clear();
-    }
-
-    @Override
-    public Set<String> keySet() {
-        return map.keySet();
-    }
-
-    @Override
-    public Collection<UserCredential> values() {
-        return map.values();
-    }
-
-    @Override
-    public Set<java.util.Map.Entry<String, UserCredential>> entrySet() {
-        return map.entrySet();
     }
 
     @Override
