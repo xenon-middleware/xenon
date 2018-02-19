9910e0e01a6b5179fafdf105df34d7690ea054d3
Jason Maassen <jmaassen73@gmail.com>
2018-02-19 11:17:29 +0100

    Added unit test for memory and name settings

--- src/test/java/nl/esciencecenter/xenon/adaptors/schedulers/slurm/SlurmUtilsTest.java
+++ src/test/java/nl/esciencecenter/xenon/adaptors/schedulers/slurm/SlurmUtilsTest.java
@@ -713,6 +713,24 @@ public class SlurmUtilsTest {
     }
 
     @Test
+    public void test_generateInterActiveCPUsPerTask() {
+        Path entry = new Path("/entry");
+        UUID tag = new UUID(0, 42);
+
+        JobDescription description = new JobDescription();
+        description.setExecutable("exec");
+        description.setArguments(new String[] { "a", "b", "c" });
+        description.setThreadsPerProcess(4);
+
+        String[] expected = new String[] { "--quiet", "--job-name=" + tag.toString(), "--nodes=1", "--ntasks-per-node=1", "--cpus-per-task=4", "--time=15",
+                "exec", "a", "b", "c" };
+
+        String[] result = SlurmUtils.generateInteractiveArguments(description, entry, tag);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
     public void test_generateInterActiveArgumentsWithRelativeDirAndQueue() {
         Path entry = new Path("/entry");
         UUID tag = new UUID(0, 42);
